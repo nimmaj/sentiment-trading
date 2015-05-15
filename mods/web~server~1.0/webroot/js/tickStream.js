9c74bcbe3474f3c8d6f8878fc7ef8719bf7a5e15
nimmaj <git@nimmaj.com>
2015-05-15 21:49:50 +0100

    more titivation

--- mods/web~server~1.0/webroot/js/tickStream.js
+++ mods/web~server~1.0/webroot/js/tickStream.js
@@ -6,60 +6,59 @@ var graphs = new Map();
 
 function tickReceived(tickMessage) {
   var tick = JSON.parse(tickMessage.data);
-  // console.log(tick);
-  // if (tick.instrument === "EUR_USD") {
-    if (!graphs.has(tick.instrument)) {
-      console.log("adding new graph section for: "+tick.instrument);
+  
+  if (!graphs.has(tick.instrument)) {
+    console.log("adding new graph section for: "+tick.instrument);
 
-      var graphElement = document.getElementById("rateGraphs");
-      var newRow = document.createElement("div");
-      newRow.setAttribute("class","row");
+    var graphElement = document.getElementById("rateGraphs");
+    var newRow = document.createElement("div");
+    newRow.setAttribute("class","row");
 
-      var newGraphDiv = divWithClass("col-md-8");
+    var newGraphDiv = divWithClass("col-md-8");
 
-      var titleDiv = document.createElement("h5");
-      var title = document.createTextNode(tick.instrument);
-      titleDiv.appendChild(title);
+    var titleDiv = document.createElement("h5");
+    var title = document.createTextNode(tick.instrument);
+    titleDiv.appendChild(title);
 
-      var graphDiv = document.createElement("div");
+    var graphDiv = document.createElement("div");
 
-      var graphCanvas = document.createElement("canvas");
-      graphCanvas.setAttribute("id", tick.instrument);
-      graphCanvas.setAttribute("width", 660);
-      graphCanvas.setAttribute("height", 150);
-      graphDiv.appendChild(graphCanvas);
+    var graphCanvas = document.createElement("canvas");
+    graphCanvas.setAttribute("id", tick.instrument);
+    graphCanvas.setAttribute("width", 660);
+    graphCanvas.setAttribute("height", 150);
+    graphDiv.appendChild(graphCanvas);
 
-      newGraphDiv.appendChild(titleDiv);
-      newGraphDiv.appendChild(graphDiv);
+    newGraphDiv.appendChild(titleDiv);
+    newGraphDiv.appendChild(graphDiv);
 
-      newRow.appendChild(divWithClass("col-md-2"));
-      newRow.appendChild(newGraphDiv);
-      newRow.appendChild(divWithClass("col-md-2"));
+    newRow.appendChild(divWithClass("col-md-2"));
+    newRow.appendChild(newGraphDiv);
+    newRow.appendChild(divWithClass("col-md-2"));
 
-      graphElement.appendChild(newRow);
+    graphElement.appendChild(newRow);
 
 
-      var ts = {
-        bid: new TimeSeries(),
-        ask: new TimeSeries()
-      };
+    var ts = {
+      bid: new TimeSeries(),
+      ask: new TimeSeries()
+    };
 
-      graphs.set(tick.instrument, ts);
+    graphs.set(tick.instrument, ts);
 
-      var smoothie = new SmoothieChart({ grid: { strokeStyle: 'rgb(125, 0, 0)', fillStyle: 'rgb(60, 0, 0)', lineWidth: 1, millisPerLine: 1000, verticalSections: 6 },
-        labels: { precision: 4} ,
-        millisPerPixel:50});
-      smoothie.addTimeSeries(ts.bid, { strokeStyle: 'rgb(0, 255, 0)', fillStyle: 'rgba(0, 255, 0, 0.4)', lineWidth: 3 });
-      smoothie.addTimeSeries(ts.ask, { strokeStyle: 'rgb(255, 0, 255)', fillStyle: 'rgba(255, 0, 255, 0.3)', lineWidth: 3 });
+    var smoothie = new SmoothieChart({ grid: { strokeStyle: 'rgb(125, 0, 0)', fillStyle: 'rgb(60, 0, 0)', lineWidth: 1, millisPerLine: 1000, verticalSections: 6 },
+      labels: { precision: 4} ,
+      millisPerPixel:50});
+    smoothie.addTimeSeries(ts.bid, { strokeStyle: 'rgb(0, 255, 0)', fillStyle: 'rgba(0, 255, 0, 0.4)', lineWidth: 3 });
+    smoothie.addTimeSeries(ts.ask, { strokeStyle: 'rgb(255, 0, 255)', fillStyle: 'rgba(255, 0, 255, 0.3)', lineWidth: 3 });
 
-      smoothie.streamTo(document.getElementById(tick.instrument), 1000);
-    }
+    smoothie.streamTo(document.getElementById(tick.instrument), 1000);
+  }
 
-    console.log("got eur usd tick: " + tick.bid + ' ' + tick.ask);
-    var timeSeries = graphs.get(tick.instrument);
-    timeSeries.bid.append(new Date().getTime(), tick.bid);
-    timeSeries.ask.append(new Date().getTime(), tick.ask);
-  // }
+  console.log("got eur usd tick: " + tick.bid + ' ' + tick.ask);
+  var timeSeries = graphs.get(tick.instrument);
+  timeSeries.bid.append(new Date().getTime(), tick.bid);
+  timeSeries.ask.append(new Date().getTime(), tick.ask);
+
 }
 
 function divWithClass(className) {
