d771d52ed077986cc57c448f58939270fa991924
nimmaj <git@nimmaj.com>
2015-05-15 16:33:46 +0100

    introduced a mode flag

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ var config = container.config;
 depMod('oanda~stream~1.0', config.stream);
 depMod('web~server~1.0', config.web);
 depMod('io.vertx~mod-mongo-persistor~2.1.0', config.mongo);
-depMod('sentiment~tester~1.0', config.web);
 depMod('sentiment~repository~1.0', config.mongo);
+
+if (config.mode === "test") {
+  depMod('sentiment~tester~1.0', config.web);
+}
 
 // anyone else think that container.deployModule ought to do this?!!
 function depMod(name, conf) {
