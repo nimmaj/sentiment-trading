ed649d53c8982d036de016718e88c01af408a67b
nimmaj <git@nimmaj.com>
2015-05-16 20:09:31 +0100

    pub time - think that's the republishing going

--- mods/sentiment~repository~1.0/service.js
+++ mods/sentiment~repository~1.0/service.js
@@ -6,16 +6,55 @@ var moment = require('./lib/moment.js');
 var eb = vertx.eventBus;
 var config = container.config;
 
-console.log('starting sentiment repository...');
+var sentimentMode = config.mode;
 
-eb.registerHandler('sentiment.event', function(message) {
-  console.log('storing: '+JSON.stringify(message));
+console.log('starting sentiment repository in ' + sentimentMode + ' mode...');
 
-  var mm = {
-    "action": "save",
-    "collection": "event",
-    "document": message
-  }
+// { "timestamp": { $gt: "2015-05-15T15:57:50.000Z", $lt: "2015-05-15T15:57:59.000Z" } }
 
-  eb.send(config.address, mm);
-});
+if (sentimentMode === 'store') {
+  eb.registerHandler('sentiment.event', function(message) {
+    console.log('storing: '+JSON.stringify(message));
+
+    var mm = {
+      "action": "save",
+      "collection": "event",
+      "document": message
+    }
+
+    eb.send(config.address, mm);
+  });
+} else {
+  var looped = false;
+  var previousTick = "";
+  eb.registerHandler('timer.tick', function(event) {
+    if (looped) {
+      var search = {
+        "timestamp": {
+          "$gt": convDate(previousTick),
+          "$lte": convDate(event)
+        }
+      };
+
+      var sm = {
+        "action": "find",
+        "collection": "event",
+        "matcher": search
+      }
+
+      eb.send(config.address, sm, function(reply) {
+        if (reply.number > 0) {
+          reply.results.forEach(function(result) {
+            eb.publish('sentiment.event', result);
+          });
+        }
+      });
+    }
+    previousTick = event;
+    looped = true;
+  });
+}
+
+function convDate(dt) {
+  return moment(dt).format('YYYY-MM-DD HH:mm:ss');
+}
